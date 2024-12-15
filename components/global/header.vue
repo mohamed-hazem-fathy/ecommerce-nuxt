5ebe6fbb266c28c4fa60f1347c5e7a5368715db8
Mohamed Hazem <125741147+mohamed-hazem-fathy@users.noreply.github.com>
2024-12-15 13:05:18 +0200

    add

--- components/global/header.vue
+++ components/global/header.vue
@@ -16,42 +16,54 @@
 
 <style lang="scss" >
 .header {
-  position: relative;
+  top: 0px;
+
 }
 .img {
-  background-image: url('/assets/imges/PromotionImage.png');
+
   background-size: cover;
   background-position: center;
   width: 100%;
   height: 800px; // الطول على الديسكتوب
   position: relative;
 
-  @media (max-width: 768px) {
-    // عند الموبايل، نعدل الصورة لتظهر كاملة
-    background-size: contain; // تظهر الصورة كاملة
+  @media (max-width: 800px) {
+    background-size: contain;
+    border-radius: 4px;
     background-repeat: no-repeat;
-    height: 400px; // ارتفاع أصغر على الموبايل
+    height: 350px;
     background-position: center center;
     background-image: url('/assets/imges/Promotion Image.png');
-    top: -80px;
-
+    top:0px;
+  }
+  @media (min-width:1024px) {
+    background-image: url('/assets/imges/PromotionImage.png');
+   
   }
 }
 h1 {
   position: absolute;
   top: 50%;
   font-size: 100px;
-  margin-left: 30px;
+  margin-left: 70px;
   @media (max-width: 800px) {
     font-size: 24px; // نص أصغر على شاشات الموبايل
     top: 30%;
   }
 }
-@media (min-width: 768px) and (max-width: 1024px) {
+@media (min-width: 800px) and (max-width: 1024px) {
   .img {
     background-size: contain;
     background-image: url('/assets/imges/Promotion Image.png');
-    margin-top: 0;
+   top: -115px;
+  }
+
+  h1 {
+    position: absolute;
+    top: 30%;
+    font-size:50px;
+    margin-left: 30px;
+
   }
 }
 
